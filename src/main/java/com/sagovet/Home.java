17529629987c0c35071303b11aab5fef69b7a208
Nis Hua Van <huavannis@gmail.com>
2018-11-09 10:56:54 +0100

    Add dependancies

---
+++ src/main/java/com/sagovet/Home.java
@@ -0,0 +1,14 @@
+package com.sagovet;
+
+import java.awt.HeadlessException;
+import javax.swing.JFrame;
+
+/**
+ * @author hvnis
+ */
+public class Home extends JFrame {
+    public Home() throws HeadlessException
+    {
+        setSize(800, 600);
+    }
+}
---
+++ src/main/java/com/sagovet/Main.java
@@ -0,0 +1,11 @@
+package com.sagovet;
+
+/**
+ * @author hvnis
+ */
+public class Main {
+    public static void main(String[] args) {
+        Home home = new Home();
+        home.setVisible(true);
+    }
+}
